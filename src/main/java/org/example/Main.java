7c2ad75c4c601c8ea6a76509a8c9475573f70e40
Heide-00 <gulaslan93@gmail.com>
2025-07-19 22:14:16 +0300

    Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import org.example.enums.Plan;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+     runApp();
+    }
+
+    public static void runApp(){
+        String[] employees=new String[2];
+        Company company=new Company(1,"Workintech",1000.0,employees);
+        company.addEmployee(0,"Jane Doe");
+
+        String[] healths=new String[3];
+        Employee employee=new Employee(30,"Ayşe Güçlü","ayseaslan@gmail.com","bmkhgj",healths);
+        employee.addHealthPlan(-1,"Test Sigorta");
+
+        Healthplan healthplan=new Healthplan(200,"Standart Health Coverage", Plan.BASIC);
+
+        Plan selectedPlan=Plan.PREMIUM;
+
+        System.out.println(company);
+        System.out.println(employee);
+        System.out.println(healthplan);
+        System.out.println("Selected Plan: " + selectedPlan);
     }
 }
